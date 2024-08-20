68c98c954c05c9893586e3d8b5738c4bd134a6c0
mrxzeta <oscau78@gmail.com>
2024-08-20 12:28:10 -0600

    nuevos

--- assets/js/compra.js
+++ assets/js/compra.js
@@ -37,7 +37,7 @@ function procesarCompra(e){
           })
     }
     else{
-        emailjs.init('user_hx2rYaxbexZ0qlT8bs771')
+        emailjs.init('sXboaDmrtf4VvKMFv')
 
         const btn = document.getElementById('procesar-compra');
 
@@ -54,7 +54,8 @@ function procesarCompra(e){
             enviado.width = '150';
 
         const serviceID = 'default_service';
-        const templateID = 'template_rtfpoq5';
+        const templateID = 'template_t9d9ord';
+        
 
         emailjs.sendForm(serviceID, templateID, this)
             .then(() => {
